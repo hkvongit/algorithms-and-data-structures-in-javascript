2f55977318702f17410f1661dc7e9257e3fab32f
hariFBD <harikrishnan@firstbytedigital.com>
2019-09-15 18:39:24 +0530

    string count best logic

---
+++ string count best logic.js
@@ -0,0 +1,19 @@
+
+dummy = "haLle all".split(" ");
+duppi = dummy.join("");
+ary = Array.from(duppi);
+
+ob = {};
+const func = (ary,ob) => {
+  for (ele in ary) {
+      key = ary[ele]
+    if (ob[key] === undefined) {
+      ob[key] = 1;
+    } else {
+      ob[key] += 1;
+    }
+  }
+};
+
+func(ary, ob);
+console.log(ob);
