70106642fa79c631e16ec276e339a0a23eb6fa2c
hkvongit <brosgiant@gmail.com>
2021-07-29 00:27:35 +0530

    Included Insert, Remove, Get Nodes by index.

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -27,8 +27,55 @@ class LinkedList {
     }
     return current.value;
   }
-}
 
+  getNodeByIndex(index) {
+    if (index >= this.length || index < 0) return null
+    let current = this.head
+    for (let i = 0; i < index; i++) {
+      current = current.next
+    }
+    return current;
+  }
+
+  insertAtIndex(index, value) {
+    if (index === 0) return this.insertAtHead(value)
+    if (index > this.length || index > this.length) return null
+    if (index > 0 && index <= this.length) {
+      const prev = this.getNodeByIndex(index - 1);
+      prev.next = new LinkedListNode(value, prev.next)
+      this.length++
+    }
+  }
+
+  removeHead() {
+    this.head = this.head.next;
+    this.length--
+  }
+
+  removeNodeAtIndex(index) {
+    if (this.length >= 0 && index >=0) {
+      if (index === 0) {
+        this.removeHead()
+        return
+      }
+      if (index < this.length) {
+        const prevNode = this.getNodeByIndex(index - 1);
+        if (index === this.length - 1) {
+          prevNode.next = null
+        }
+        else {
+          const current = this.getNodeByIndex(index);
+          prevNode.next = current.next;
+        }
+        this.length--;
+        return
+      }
+    }
+    console.log(`Node at index=${index} doesnt' exist`)
+    return
+  }
+
+}
 // This class represents a node in the linked list
 class LinkedListNode {
   constructor(value, next) {
--- linked-list/linked-list.test.js
+++ linked-list/linked-list.test.js
@@ -1,5 +1,6 @@
 // const { expect } = require('@jest/globals');
 // const { describe } = require('yargs');
+const { expect } = require('@jest/globals');
 const LinkedList = require('./linked-list');
 
 describe('#insertAtHead', () => {
@@ -57,4 +58,113 @@ describe('#getElementByIndex', () => {
       expect(ll.getValueByIndex(1)).toBe(22);
     })
   })
+})
+
+describe('#insertAtIndex', () => {
+
+  describe('insert at index 0', () => {
+    test('insert at given index', () => {
+
+      const ll = new LinkedList();
+      ll.insertManyFromArray([11, 12, 13])
+      ll.insertAtIndex(0, 30)
+      expect(ll.getValueByIndex(0)).toBe(30)
+    })
+  })
+
+  describe('insert at end of list', () => {
+    test('insert at given index', () => {
+
+      const ll = new LinkedList();
+      ll.insertManyFromArray([11, 12, 13])
+      const length = ll.length
+      ll.insertAtIndex(length, 30)
+      expect(ll.getValueByIndex(length)).toBe(30)
+    })
+  })
+
+  describe('insert at position less than 0', () => {
+    test('insert at given index', () => {
+
+      const ll = new LinkedList();
+      ll.insertManyFromArray([11, 12, 13])
+      ll.insertAtIndex(-1, 30)
+      expect(ll.getValueByIndex(-1)).toBe(null)
+    })
+  })
+
+
+  describe('insert at position greater than length', () => {
+    test('insert at given index', () => {
+
+      const ll = new LinkedList();
+      ll.insertManyFromArray([11, 12, 13])
+      ll.insertAtIndex(5, 30)
+      expect(ll.getValueByIndex(5)).toBe(null)
+    })
+  })
+
+  describe('insert at middle position', () => {
+    test('insert at given index', () => {
+
+      const ll = new LinkedList();
+      ll.insertManyFromArray([11, 12, 14, 15])
+      ll.insertAtIndex(2, 30)
+      expect(ll.getValueByIndex(2)).toBe(30)
+    })
+  })
+})
+
+describe('#removeNodeAtIndex', () => {
+  describe("removing element from head",()=>{
+
+    test('removes node at a particular index', ()=>{
+      const ll = new LinkedList()
+      ll.insertManyFromArray([10, 20, 30])
+      ll.removeNodeAtIndex(0) 
+      expect(ll.getValueByIndex(0)).toBe(20) 
+    })
+  })
+
+  describe("removing element from end",()=>{
+
+    test('removes node at a particular index', ()=>{
+      const ll = new LinkedList()
+      ll.insertManyFromArray([10, 20, 30])
+      ll.removeNodeAtIndex(2)
+      const length = ll.length 
+      expect(ll.getValueByIndex(length - 1)).toBe(20) 
+    })
+  })
+
+  describe("removing element from middle",()=>{
+
+    test('removes node at a particular index', ()=>{
+      const ll = new LinkedList()
+      ll.insertManyFromArray([10, 20, 30])
+      ll.removeNodeAtIndex(1)
+      expect(ll.getValueByIndex(1)).toBe(30) 
+    })
+  })
+  
+  describe("removing element from non existing index",()=>{
+
+    test('removes node at negative index', ()=>{
+      const ll = new LinkedList()
+      ll.insertManyFromArray([10, 20, 30])
+      ll.removeNodeAtIndex(-2)
+      const length = ll.length 
+      expect(ll.getValueByIndex(-2)).toBeNull
+    })
+
+    test('removes node at above length', ()=>{
+      const ll = new LinkedList()
+      ll.insertManyFromArray([10, 20, 30])
+      ll.removeNodeAtIndex(5)
+      const length = ll.length 
+      expect(ll.getValueByIndex(5)).toBeNull
+    })
+    
+  })
+
 })
